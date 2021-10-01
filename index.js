41f34434d514ff6bb5b1068050cbb89483a3ec96
Ritvik Biswas <ritvik.biswas@gmail.com>
2021-10-01 15:43:41 -0400

    one endpoint created

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
 const app = express();
 
 
@@ -6,8 +8,32 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/about", (req, res) => {
-    res.send("About route");
+app.get("/fxapi/:date", async (req, res) => {
+    //console.log(req.params.date);
+    //console.log(req.query.from);
+    const date = req.params.date;
+    const from = req.query.from;
+    try{
+        const rates = await getRates(date, from);
+        res.send(rates);
+    }
+    catch(error){
+        res.status(400).send({message: "Invalid input. Please check your URL and try again."});
+    }
 });
+
+/**
+ * Helper Functions
+ */
+const getRates = async (date, from) => {
+    const api_url = "https://api.frankfurter.app/" + date + "?from=" + from; //url to get the rate for the currency in "from"
+    try{
+        const response = await axios.get(api_url);
+        return response.data;
+    }
+    catch(error){
+        throw new Error(error);
+    }
+};
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
