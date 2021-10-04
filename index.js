c18edd1d8fe8084740548ac952b587b81ea3e839
Your Name <you@example.com>
2021-10-04 11:22:13 -0400

    test

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ const convert = async (date, amount, from) => {
     catch(error){
         throw new Error(error);
     }
+    console.log("Adding this random change...");
 }
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
