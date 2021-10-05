0f8e30c64609883373b5f69bc99d59d82edc50c0
Your Name <you@example.com>
2021-10-05 14:56:27 -0400

    API Bug fixed where the FOREX API being utilized was not properly sending back 404s, and errors were not being returned to handle. Setup a custom timeout to timeout request after 5 seconds and return the error.

--- index.js
+++ index.js
@@ -5,70 +5,71 @@ const qs = require("qs");
 const app = express();
 
 app.use(
-  cors({
-    origin: "*",
-  })
+    cors({
+        origin: "*",
+    })
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.get("/fxapi/:date", async (req, res) => {
-  console.log("Get Rates!");
-  //console.log(req.params.date);
-  //console.log(req.query.from);
-  const date = req.params.date;
-  const from = req.query.from;
-  try {
-    const rates = await getRates(date, from);
-    res.send(rates);
-  } catch (error) {
-    res
-      .status(400)
-      .send({ message: "Invalid input. Please check your URL and try again." });
-  }
+    console.log("Get Rates!");
+    //console.log(req.params.date);
+    //console.log(req.query.from);
+    const date = req.params.date;
+    const from = req.query.from;
+    try {
+        const rates = await getRates(date, from);
+        res.send(rates);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            message: "Invalid input. Please check your URL and try again.",
+        });
+    }
 });
 
 app.get("/fxapi/convert/:date", async (req, res) => {
-  console.log("Convert!");
-  const date = req.params.date;
-  const amount = req.query.amount;
-  const from = req.query.from;
+    console.log("Convert!");
+    const date = req.params.date;
+    const amount = req.query.amount;
+    const from = req.query.from;
 
-  try {
-    const rates = await convert(date, amount, from);
-    res.send(rates);
-  } catch (error) {
-    //console.log(error);
-    res.status(400).send({ message: "Invalid input. Please check" });
-  }
+    try {
+        const rates = await convert(date, amount, from);
+        res.send(rates);
+    } catch (error) {
+        //console.log(error);
+        res.status(400).send({ message: "Invalid input. Please check" });
+    }
 });
 
 app.get("/fxapi/average/:from", async (req, res) => {
-  console.log("Average!");
-  const startDate = new Date(req.query.start);
-  const dupStart = new Date(startDate);
-  const endDate = new Date(req.query.end);
-  const from = req.params.from;
-  //console.log(startDate);
-  //console.log(endDate);
-  //console.log(from);
+    console.log("Average!");
+    const startDate = new Date(req.query.start);
+    const dupStart = new Date(startDate);
+    const endDate = new Date(req.query.end);
+    const from = req.params.from;
+    //console.log(startDate);
+    //console.log(endDate);
+    //console.log(from);
 
-  try {
-    const rates = await average(startDate, endDate, from);
-    const objSend = {};
-    objSend.base = from;
-    objSend.startDate = dupStart.toISOString().slice(0, 10);
-    objSend.endDate = endDate.toISOString().slice(0, 10);
-    objSend.rates = rates;
-    res.send(objSend);
-  } catch (error) {
-    console.log(error);
-    res
-      .status(400)
-      .send({ message: "Invalid input. Please check your URL and try again" });
-  }
+    try {
+        const rates = await average(startDate, endDate, from);
+        const objSend = {};
+        objSend.base = from;
+        objSend.startDate = dupStart.toISOString().slice(0, 10);
+        objSend.endDate = endDate.toISOString().slice(0, 10);
+        objSend.rates = rates;
+        res.send(objSend);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            message: "Invalid input. Please check your URL and try again",
+        });
+    }
 });
 
 /**
@@ -76,72 +77,101 @@ app.get("/fxapi/average/:from", async (req, res) => {
  */
 //Takes as many objects as you want and adds them using reduce - https://stackoverflow.com/a/42488360
 function sumObjectsByKey(...objs) {
-  return objs.reduce((a, b) => {
-    for (let k in b) {
-      if (b.hasOwnProperty(k)) a[k] = (a[k] || 0) + b[k];
-    }
-    return a;
-  }, {});
+    return objs.reduce((a, b) => {
+        for (let k in b) {
+            if (b.hasOwnProperty(k)) a[k] = (a[k] || 0) + b[k];
+        }
+        return a;
+    }, {});
 }
 
 const getRates = async (date, from) => {
-  const api_url = "https://api.frankfurter.app/" + date + "?from=" + from; //url to get the rate for the currency in "from"
-  console.log(api_url);
-  try {
-    const response = await axios.get(api_url);
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
+    console.log("Getting rates...");
+    const api_url = "https://api.frankfurter.app/" + date + "?from=" + from; //url to get the rate for the currency in "from"
+    //const api_url = "https://edgedev.edgetg.com/";
+    console.log(api_url);
+
+    const source = axios.CancelToken.source();
+    const timeout = setTimeout(() => {
+        source.cancel();
+    }, 5000);
+
+    try {
+        const response = await axios.get(api_url, { cancelToken: source.token });
+        clearTimeout(timeout);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw new Error(error);
+    }
 };
 
 const convert = async (date, amount, from) => {
-  const api_url =
-    "https://api.frankfurter.app/" +
-    date +
-    "?amount=" +
-    amount +
-    "&from=" +
-    from; //url to get the conversion for amount in "amount" and the currency in "from"
-  console.log(api_url);
-  try {
-    const response = await axios.get(api_url);
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
+    const api_url =
+        "https://api.frankfurter.app/" +
+        date +
+        "?amount=" +
+        amount +
+        "&from=" +
+        from; //url to get the conversion for amount in "amount" and the currency in "from"
+    console.log(api_url);
+
+    const source = axios.CancelToken.source();
+    const timeout = setTimeout(() => {
+        source.cancel();
+    }, 5000);
+
+    try {
+        const response = await axios.get(api_url, { cancelToken: source.token });
+        clearTimeout(timeout);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw new Error(error);
+    }
 };
 
 const average = async (startDate, endDate, from) => {
-  let dateItr = startDate;
-  let count = 0;
-  let ratesAggregated = {};
-  while (dateItr.getTime() <= endDate.getTime()) {
-    let dateStr = dateItr.toISOString().slice(0, 10);
-    const api_url = "https://api.frankfurter.app/" + dateStr + "?from=" + from; //url to get the rate for the currency in "from"
-    console.log(api_url);
+    let dateItr = startDate;
+    let count = 0;
+    let ratesAggregated = {};
+    while (dateItr.getTime() <= endDate.getTime()) {
+        let dateStr = dateItr.toISOString().slice(0, 10);
+        const api_url =
+            "https://api.frankfurter.app/" + dateStr + "?from=" + from; //url to get the rate for the currency in "from"
+        console.log(api_url);
 
-    //make req
-    try {
-      const response = await axios.get(api_url);
-      //console.log(response.data);
-      ratesAggregated = sumObjectsByKey(ratesAggregated, response.data.rates);
-      count++;
-    } catch (error) {
-      throw new Error(error);
+        const source = axios.CancelToken.source();
+        const timeout = setTimeout(() => {
+            source.cancel();
+        }, 5000);
+
+        //make req
+        try {
+            const response = await axios.get(api_url, { cancelToken: source.token });
+            //console.log(response.data);
+            ratesAggregated = sumObjectsByKey(
+                ratesAggregated,
+                response.data.rates
+            );
+            count++;
+            clearTimeout(timeout);
+        } catch (error) {
+            throw new Error(error);
+        }
+
+        dateItr.setDate(dateItr.getDate() + 1);
     }
 
-    dateItr.setDate(dateItr.getDate() + 1);
-  }
+    //console.log(ratesAggregated);
 
-  //console.log(ratesAggregated);
+    Object.entries(ratesAggregated).forEach(([key, value]) => {
+        ratesAggregated[key] = value / count;
+    });
 
-  Object.entries(ratesAggregated).forEach(([key, value]) => {
-    ratesAggregated[key] = value / count;
-  });
-
-  //console.log(ratesAggregated);
-  return ratesAggregated;
+    //console.log(ratesAggregated);
+    return ratesAggregated;
 };
 
-app.listen(3000, () => console.log("Server is listening on port 3000"));
+const port = process.env.PORT || 3000;
+var server = app.listen(3000, "localhost", () => console.log("Server is listening on port " + server.address().port));
